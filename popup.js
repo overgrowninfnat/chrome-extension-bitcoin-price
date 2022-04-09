b759891c7e1068777ec15b48d20176a3bba3340e
theItalianDev <italiandev@protonmail.com>
2022-04-09 10:15:11 +0200

    final code

--- popup.js
+++ popup.js
@@ -8,24 +8,4 @@ chrome.storage.sync.get(["bitcoin"], (result) => {
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
   priceEl.textContent = `$ ${changes.bitcoin.newValue[0].current_price}`;
-  if (
-    changes.bitcoin.newValue[0].current_price >
-    changes.bitcoin.oldValue[0].current_price
-  ) {
-    chrome.action.setBadgeText({
-      text: "UP",
-    });
-    chrome.action.setBadgeBackgroundColor({
-      color: "green",
-    });
-    priceEl.style.color = "green";
-  } else {
-    chrome.action.setBadgeText({
-      text: "DOWN",
-    });
-    chrome.action.setBadgeBackgroundColor({
-      color: "red",
-    });
-    priceEl.style.color = "red";
-  }
 });
